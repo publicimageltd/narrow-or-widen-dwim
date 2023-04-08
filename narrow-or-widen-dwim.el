a2097dfdfd296c9684f1b8d3cdd51d3e1edec34d
Jörg Volbers <joerg@joergvolbers.de>
2023-04-08 17:31:49 +0200

    Clean up

--- narrow-or-widen-dwim.el
+++ narrow-or-widen-dwim.el
@@ -33,30 +33,18 @@
   #'split-window-horizontally
   "Display cloned buffers in a window returned by that function.")
 
-  
 ;;; * Declare major mode specific functions
 
 ;; Declare all major mode specific functions to avoid requir'ing them
 ;; all. They will be called only if the mode is loaded, anyways.
 
-(declare-function org-narrow-to-block
-		  "org"
-		  ())
-(declare-function org-narrow-to-subtree
-		  "org"
-		  ())
-(declare-function org-edit-src-code
-		  "org-src"
-		  (&optional code edit-buffer-name))
-(declare-function outline-up-heading
-		  "outline"
-		  (arg &optional invisible-ok))
-(declare-function LaTeX-narrow-to-environment
-		  "latex"
-		  (&optional COUNT))
-(declare-function narrow-to-defun
-		  "lisp"
-		  (&optional INCLUDE-COMMENTS))
+(declare-function org-narrow-to-block "org" ())
+(declare-function org-narrow-to-subtree "org" ())
+(declare-function org-before-first-heading-p "org" ())
+(declare-function org-edit-src-code "org-src" (&optional code edit-buffer-name))
+(declare-function outline-up-heading "outline" (arg &optional invisible-ok))
+(declare-function LaTeX-narrow-to-environment "latex" (&optional COUNT))
+(declare-function narrow-to-defun "lisp" (&optional INCLUDE-COMMENTS))
 
 ;;; * Narrow or widen dwim
 
@@ -67,18 +55,22 @@ The new window is created by calling the function stored in
 the window in which the cloned buffer should be displayed."
   (when buf
     (if (get-buffer-window buf)
-	(switch-to-buffer buf)
+        (switch-to-buffer buf)
       (select-window (funcall narrow-or-widen-dwim-window-split-function))
       (switch-to-buffer buf t t))))
 
 (defun narrow-or-widen--org-narrow (&optional arg)
   "In org mode, dwim-narrow.
 Prefix ARG is used to narrow to the next 'upper' top tree level.
-The numeric argument is interpreted unorthodoxically: One
-prefix (C-u) means 'one top level up', two prefixes (C-u C-u)
-means 'two levels up', etc. Real numeric arguments (like C-u 3)
-should not be used; they are divided by four and rounded up to
-determine the number of levels 'up'"
+
+The numeric argument is interpreted the following: One prefix
+\\[universal-argument] means 'narrow on one top level up', two
+prefixes (\\[universal-argument]\\[universal-argument]) means
+'two levels up', etc.
+
+Real numeric arguments (like \\[universal-argument] 3) should not
+be used; they are divided by four and rounded up to determine the
+number of levels 'up'."
   (unless (mod arg 4)
     (user-error "Prefix arg has to be a multiple of 4"))
   (cond
@@ -88,60 +80,61 @@ determine the number of levels 'up'"
    (t
     (progn
       (save-excursion
-	(when (> arg 1)
-	  (outline-up-heading (ceiling (/ arg 4)) t))
+        (when (> arg 1)
+          (outline-up-heading (ceiling (/ arg 4)) t))
         (if (org-before-first-heading-p)
             (user-error "Point not at a subtree, cannot narrow")
-	  (org-narrow-to-subtree)))))))
+          (org-narrow-to-subtree)))))))
 
 ;;;###autoload
 (defun narrow-or-widen-dwim (&optional n)
     "Widen if buffer is narrowed, narrow-dwim otherwise.
-Dwim means: region, org-src-block, org-subtree, or
-defun, whichever applies first."
+Dwim means: region, org-src-block, org-subtree, or defun,
+whichever applies first. Optional prefix argument N is used to
+widen the scope of the narrowed region."
   (interactive "p")
   (declare (interactive-only))
   (cond (;; widen, if narrowed:
-	 (buffer-narrowed-p)
-	 (progn
-	   (widen)
-	   (when narrow-or-widen-dwim--window-start
-	     (set-window-start (selected-window) narrow-or-widen-dwim--window-start t)
-	     (setq narrow-or-widen-dwim--window-start nil))))
-	;; narrow to region, if defined:
-	((region-active-p)
-	 (progn
-	   (setq narrow-or-widen-dwim--window-start (window-start))
-	   (narrow-to-region (region-beginning) (region-end))
-	   (deactivate-mark)
-	   (goto-char (point-min))))
-	;; special handling in org mode buffers:
-	((derived-mode-p 'org-mode)
-	 (progn
-	   (setq narrow-or-widen-dwim--window-start (window-start))
-	   (narrow-or-widen--org-narrow n)))
-	;; special handling in latex mode buffers:
-	((derived-mode-p 'latex-mode)
-	 (progn
-	   (setq narrow-or-widen-dwim--window-start (window-start))
-	   (LaTeX-narrow-to-environment n)))
-	;; special handling in prog mode buffers:
-	((derived-mode-p 'prog-mode)
-	 (let* ((arg (not (eq n 1)))
-		(narrow-to-defun-include-comments arg))
-	   (setq narrow-or-widen-dwim--window-start (window-start))
-	   (narrow-to-defun arg)))
-	;; else we don't know what to do:
-	(t (user-error "No suitable narrowing command available for this major mode"))))
+         (buffer-narrowed-p)
+         (progn
+           (widen)
+           (when narrow-or-widen-dwim--window-start
+             (set-window-start (selected-window) narrow-or-widen-dwim--window-start t)
+             (setq narrow-or-widen-dwim--window-start nil))))
+        ;; narrow to region, if defined:
+        ((region-active-p)
+         (progn
+           (setq narrow-or-widen-dwim--window-start (window-start))
+           (narrow-to-region (region-beginning) (region-end))
+           (deactivate-mark)
+           (goto-char (point-min))))
+        ;; special handling in org mode buffers:
+        ((derived-mode-p 'org-mode)
+         (progn
+           (setq narrow-or-widen-dwim--window-start (window-start))
+           (narrow-or-widen--org-narrow n)))
+        ;; special handling in latex mode buffers:
+        ((derived-mode-p 'latex-mode)
+         (progn
+           (setq narrow-or-widen-dwim--window-start (window-start))
+           (LaTeX-narrow-to-environment n)))
+        ;; special handling in prog mode buffers:
+        ((derived-mode-p 'prog-mode)
+         (let* ((arg (not (eq n 1)))
+                (narrow-to-defun-include-comments arg))
+           (setq narrow-or-widen-dwim--window-start (window-start))
+           (narrow-to-defun arg)))
+        ;; else we don't know what to do:
+        (t (user-error "No suitable narrowing command available for this major mode"))))
 
 ;;;###autoload
 (defun narrow-or-widen-dwim-clone (&optional n)
-  "Do `narrow-or-widen-dwim' in a new indirect buffer.
-Returns the new buffer."
+  "Clone buffer and do `narrow-or-widen-dwim'.
+Pass N to `narrow-or-widen-dwim'. Return the new buffer."
   (interactive "p")
   (declare (interactive-only))
   (when (buffer-narrowed-p)
-    (user-error "You have to widen the buffer to clone something"))
+    (user-error "You have to widen the buffer to clone it"))
   (let* ((calling-buffer (current-buffer))
          (cloned-buffer (clone-indirect-buffer nil nil t)))
     (narrow-or-widen--switch-buffer-niceley cloned-buffer)
@@ -154,13 +147,13 @@ Returns the new buffer."
 
 ;;;###autoload
 (defun narrow-or-widen-dwim-stash (&optional n)
-  "Move the entity at point in a new buffer."
+  "Move the entity at point in a new buffer.
+Prefix argument N widens the scope of the entity."
   (interactive "p")
   (declare (interactive-only))
   (when (buffer-narrowed-p)
     (user-error "You have to widen the buffer to stash something"))
-  (let* ((orig-buf (current-buffer))
-         (orig-name (buffer-name))
+  (let* ((orig-name (buffer-name))
          (new-buf (narrow-or-widen-dwim-clone n)))
     (with-current-buffer new-buf
       (let* ((content (buffer-string)))
